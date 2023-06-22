433ebb2039a81171a92d6749ff89299f4a721e00
Bishal Wagle <bishalw@gmail.com>
2023-06-22 15:01:05 -0700

    Initial Commit

---
+++ OwnGpt/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  OwnGpt
+//
+//  Created by Bishalw on 6/22/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ OwnGpt/OwnGptApp.swift
@@ -0,0 +1,17 @@
+//
+//  OwnGptApp.swift
+//  OwnGpt
+//
+//  Created by Bishalw on 6/22/23.
+//
+
+import SwiftUI
+
+@main
+struct OwnGptApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
