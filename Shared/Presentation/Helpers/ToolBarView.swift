7d075ac98ecaf1625286c228e96d79bd1f8e820e
Bishal Wagle <bishalw@gmail.com>
2024-07-27 12:16:28 -0700

    Added ToolBarView for customizing toolbarcontent for reusability

---
+++ Shared/Presentation/Helpers/ToolBarView.swift
@@ -0,0 +1,18 @@
+//
+//  ToolBarView.swift
+//  OwnGpt
+//
+//  Created by Bishalw on 7/27/24.
+//
+
+import SwiftUI
+
+struct ToolBarView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ToolBarView()
+}
